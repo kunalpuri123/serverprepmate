56cfcdbb87f2a1e4f9953fbe51be55b81f5fcd19
Kunal Puri <112803004+kunalpuri123@users.noreply.github.com>
2025-02-08 21:10:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://localhost:3000"
+    "http://localhost:3000"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
