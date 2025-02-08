9ac5d050e9b102c6cf46a492ed06210895be9c95
Kunal Puri <112803004+kunalpuri123@users.noreply.github.com>
2025-02-08 20:46:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 4000; // Provide a default port
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-}));
+app.use(cors({ origin: false }));
 
 const razorpay = new Razorpay({ // Initialize Razorpay outside the route handler
   key_id: process.env.RAZORPAY_KEY_ID,
