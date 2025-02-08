8035d809c5e58aef90a1ff5a1169c9dafa3fdab6
Kunal Puri <112803004+kunalpuri123@users.noreply.github.com>
2025-02-09 00:06:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
